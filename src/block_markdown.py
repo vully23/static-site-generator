4c2cd3933e802befaa091425d4eaf310129748ea
vully23 <vullynerable@gmail.com>
2025-04-06 19:28:27 -0400

    added function to assign BlockTypes to markdown blocks

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,14 @@
+import re
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     final_blocks = []
@@ -6,3 +17,40 @@ def markdown_to_blocks(markdown):
         if block != "":
             final_blocks.append(block)
     return final_blocks
+
+def check_if_quote_block(block):
+    split_lines = block.splitlines()
+    for line in split_lines:
+        if not line.startswith(">"):
+            return False
+    return True
+
+def check_if_unordered_list(block):
+    split_lines = block.splitlines()
+    for line in split_lines:
+        if not line.startswith("- "):
+            return False
+    return True
+
+def check_if_ordered_list(block):
+    split_lines = block.splitlines()
+    count = 1
+    for line in split_lines:
+        if not line.startswith(f"{count}. "):
+            return False
+        count += 1
+    return True
+
+def block_to_block_type(block):
+    if re.match(r"#{1,6} \S", block):
+        return BlockType.HEADING
+    elif block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+    elif check_if_quote_block(block):
+        return BlockType.QUOTE
+    elif check_if_unordered_list(block):
+        return BlockType.UNORDERED_LIST
+    elif check_if_ordered_list(block):
+        return BlockType.ORDERED_LIST
+    else:
+        return BlockType.PARAGRAPH
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,5 +1,5 @@
 import unittest
-from block_markdown import markdown_to_blocks
+from block_markdown import markdown_to_blocks, check_if_quote_block, check_if_ordered_list, check_if_unordered_list, block_to_block_type, BlockType
 
 class TestBlockMarkdown(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -45,6 +45,19 @@ This is the same paragraph on a new line
             ],
         )
 
+    def test_block_to_block_types(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+        block = "- list\n- items"
+        self.assertEqual(block_to_block_type(block), BlockType.UNORDERED_LIST)
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), BlockType.ORDERED_LIST)
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
 
 if __name__ == "__main__":
     unittest.main()
